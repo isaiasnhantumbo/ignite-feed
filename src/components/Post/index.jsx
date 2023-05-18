256db687026939a2d9cff9a0ee485118e2517aa2
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2023-05-18 21:45:38 +0200

    :lipstick: added post component styles

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,8 +1,26 @@
-export function Post(props) {
-    return (
-      <div>
-        <strong>{props.author}</strong>
-        <p>{props.content}</p>
+import styles from './styles.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/isaiasnhantumbo.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>WebDeveloper</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
       </div>
-    )
-  }
+    </article>
+  )
+}
